92ce0511892b5763fc62a492a3626d534601d342
Jlorden <Jamie.lorden@gmail.com>
2020-05-11 16:58:57 -0500

    more-forms

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Form from './Components/UserForm'
+import Display from './Components/Display'
+
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
+  const [firstError, setFirstError] = useState("");
+  const [lastError, setLastError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmError, setConfirmError] = useState("");
+
+
+  const handleSubmit = (e) => {
+  e.preventDefault();
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword("");
+    setConfirm("");
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row m-5">
+        <div>
+          <Form
+            fName={firstName}
+            lName={lastName}
+            email={email}
+            password={password}
+            confirm={confirm} 
+            handleFirst={setFirstName}
+            handleLast={setLastName}
+            handleEmail={setEmail}
+            handlePassword={setPassword}
+            handleConfirm={setConfirm}
+            addUser={handleSubmit}
+            setFirstError={setFirstError}
+            setLastError={setLastError}
+            setEmailError={setEmailError}
+            setPasswordError={setPasswordError}
+            setConfirmError={setConfirmError}
+          />
+        </div>
+        <div>
+          <Display
+            fName={firstError}
+            lName={lastError}
+            email={emailError}
+            password={passwordError}
+            confirm={confirmError} 
+          />
+        </div>
+      </div>
+      
     </div>
   );
 }
